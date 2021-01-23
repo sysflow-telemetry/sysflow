acaa0d8edfeb82db8d5b1de3dd56006c1e5ba1a2
Frederico Araujo <frederico.araujo@ibm.com>
2021-01-23 12:06:03 -0500

    reverting to sphinx rtd theme
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-#import sphinx_rtd_theme
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('../modules/sf-apis/py3/classes/sysflow/'))
 sys.path.insert(0, os.path.abspath('../modules/sf-apis/py3/classes/'))
 
@@ -31,8 +31,7 @@ version = u'0.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = ['sphinx_rtd_theme', 'sphinx.ext.autodoc', 'm2r2', 'sphinxcontrib.bibtex']
-extensions = ['sphinx.ext.autodoc', 'm2r2', 'sphinxcontrib.bibtex']
+extensions = ['sphinx_rtd_theme', 'sphinx.ext.autodoc', 'm2r2', 'sphinxcontrib.bibtex']
 
 # Bibitex
 bibtex_bibfiles = ['refs.bib']
@@ -56,8 +55,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-#html_theme = 'sphinx_rtd_theme'
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
